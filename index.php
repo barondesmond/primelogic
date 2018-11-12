bfbd5f775f47e99e7fb5fd2601543440b54e8eb7
Baron Desmond <baron@desmond.com>
2018-11-12 15:24:23 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 include("_db_config.php");
 echo "test";
 
-$version  = mssql_query('SELEECT @@VERSION');
+$version  = mssql_query('SELECT @@VERSION');
 $row = mysql_fetch_array($version);
 
 echo $row[0];
