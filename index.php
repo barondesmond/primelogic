5b83d304b95ecdf980e1f377a7bac4b866583aa1
barondesmond <baron@desmond.com>
2018-11-12 15:03:24 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 <?php
-//include("main_include.php");
 
 echo "test";
 
-$con = mssql_connect('rds01\esc', 'Reports', 'Reports123!');
-$sql = "SHOW DATABASES";
-$res = mssql_query($sql);
-$db = mssql_fetch_array($res);
 print_r($db);
 
 ?>
