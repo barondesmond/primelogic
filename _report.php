71b39b40c721448c3b1eb18f88df07af9a8c5d93
Baron Desmond <baron@desmond.com>
2018-11-14 12:19:41 -0600

    Update _report.php

--- _report.php
+++ _report.php
@@ -4,7 +4,7 @@ function notes($CustNo, $day)
 
 	$day1 = $day * -1;
 	$day2 = $day1 - 30;
-	$sql = "SELECT * FROM Collectn WHERE CustNo=" . $CustNo . " and [Date] < DATEADD(DD, " . $day1 . ", getdate()) and [Date] > DATEADD(DD, " . $day2 . ", getdate()) ORDER BY [Date] DESC;";
+	$sql = "SELECT note FROM Collectn WHERE CustNo=" . $CustNo . " and [Date] < DATEADD(DD, " . $day1 . ", getdate()) and [Date] > DATEADD(DD, " . $day2 . ", getdate()) ORDER BY [Date] DESC;";
 	echo $sql;
 	//exit;
 	$res = mssql_query($sql);
