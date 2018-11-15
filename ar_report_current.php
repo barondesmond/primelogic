bedc2212f8b282e0df744bf74348d5debd219821
Baron Desmond <baron@desmond.com>
2018-11-15 08:26:34 -0600

    Update ar_report_current.php

--- ar_report_current.php
+++ ar_report_current.php
@@ -13,16 +13,16 @@ if (isset($argv[1]))
 {
 	$day = $argv[1];
 }
- if (isset($argv[2])
+ if (isset($argv[2]))
  {
 	 $emp = $argv[2];
  }
- if (isset($argv[3])
+ if (isset($argv[3]))
  {
 	 $dept = $argv[3];
  }
 
- if (isset($argv[4])
+ if (isset($argv[4]))
  {
 	 $email = $argv[4];
  }
