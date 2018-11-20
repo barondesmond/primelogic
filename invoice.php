37743c38f4c8f0311ed4dc74781214aa1ddc228d
Baron Desmond <baron@desmond.com>
2018-11-20 15:38:53 -0600

    t

--- invoice.php
+++ invoice.php
@@ -1,4 +1,7 @@
 <?php
+require ('mpdf/src/mpdf.php');
+
+
 /*
 
 invoice tables
@@ -13,29 +16,37 @@ Invoice	EquipAtt empty
 Invoice	HistLed history notes
 Invoice	History useless
 Invoice	RecDel empty
-Invoice	Receivab invoice received
+Invoice	Receivab 
 Invoice	RecLed empty
-Invoice	Sales original sales, DueDate
+Invoice	Sales CustNo, LocNo, Invoice,InvDate, EntDate, ShipName, ShipAddr1, ShipAddr2, ShipCSZ, PONum, InvAmt, DueDate
 Invoice	SalesEmp useless
 Invoice	SalesFRP empty
 Invoice	SalesLed sales notes
 Invoice	ViewListDispatches dispatch notes
 Invoice	ViewListEquipment empty
 Invoice	ViewListInvoices  Department, SalesSort
-Invoice	ViewListHistory
-Invoice	ViewReportJobReportQuotes
-Invoice	ViewReportJobReportIncome
-Invoice	ViewListDispatchesNoDetail
-Invoice	InvSign
-Invoice	ViewTableSales
+Invoice	ViewListHistory more notes
+Invoice	ViewReportJobReportQuotes empty
+Invoice	ViewReportJobReportIncome empty
+Invoice	ViewListDispatchesNoDetail more sales info
+Invoice	InvSign empty
+Invoice	ViewTableSales sales stuff
 Invoice	EmailInv
-Invoice	ViewTableSalesEmp
+Invoice	ViewTableSalesEmp nothing useful
 Invoice	ViewEquipmentDispatchInvoiceHistory
 Invoice	RecAge
 
 Description/Notes
-SELECT * FROM Dispatch WHERE Invoice = '0000019928'
 
-SELECT * FROM RecDel WHERE Invoice = '0000019928'
 
+
+SELECT Sales.Invoice, Sales.InvDate, Sales.EntDate, Sales.ShipName, Sales.ShipAddr1, Sales.ShipAddr2, Sales.ShipCSZ, Sales.PONum, Sales.InvAmount, Sales.DueDate
+FROM Sales
+WHERE Invoice = '0000011928';
+
+SELECT Paid, InvAmt-Paid as TotalDue
+FROM  Receivab
+WHERE Invoice = '0000019928';
+
+SELECT * FROM SalesLed WHERE Invoice = '0000019928' and NoPrint='0';
 */
