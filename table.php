48504be89def53f89129195e6e69e602231f6b5d
Baron Desmond <baron@desmond.com>
2018-11-21 14:52:55 -0600

    t

--- table.php
+++ table.php
@@ -2,17 +2,22 @@
 include("_pdf.php");
 $html='<html><head></head><body style="margin: 0px;">
 <style>
-
+ div.test {
+        color: #CC0000;
+        background-color: #FFFF66;
+        font-family: helvetica;
+        font-size: 10pt;
+        border-style: solid solid solid solid;
+        border-width: 2px 2px 2px 2px;
+        border-color: green #FF00FF blue red;
+        text-align: center;
+    }
 
 div.top1 {
 	color: black;
-    position: absolute;
-	z-index: 1;
-	top: 200px;
-	left: 200px;
 }
 </style>
-
+<div class="test" width="200" height="200"></div>
 <div class="top1">
 <b>
 Lafayette Co. Chancery Clerk<BR>
