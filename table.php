8951395db611f73fe15cadfbe261cb5021bfe21c
Baron Desmond <baron@desmond.com>
2018-11-21 16:19:04 -0600

    t

--- table.php
+++ table.php
@@ -11,7 +11,7 @@ $html='<html><head></head><body style="margin: 0px;">
 </style>
 <table class="first" cellpadding="0" cellspacing="0" border="0" >
  <tr>
-  <td width="200" align="left" color="grey"><BR><BR><BR><BR<BR><BR>>Office: 662.841.1390<BR>Email: service@plisolutions.com</td>
+  <td width="200" align="left" color="grey"><BR><BR><BR><BR><BR><BR<BR><BR>>Office: 662.841.1390<BR>Email: service@plisolutions.com</td>
   <td width="200" align="center"><BR><BR><BR><BR><BR></td>
   <td width="250" align="right"><table border="0" cellpadding="4" class="first"><tr><td>0000000</td></tr><tr><td>12/12/1970</td></tr><tr><td>12/12/12</td></tr><tr><td>11/11/11</td></tr><tr><td>324234</td></tr></table></td>
   <td width="50" align="right"><BR><BR><BR><BR><BR></td>
