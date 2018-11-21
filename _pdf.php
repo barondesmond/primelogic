299045848993127c53aef3828e458289a1615aaf
Baron Desmond <baron@desmond.com>
2018-11-21 15:35:44 -0600

    Update _pdf.php

--- _pdf.php
+++ _pdf.php
@@ -65,7 +65,7 @@ if (@file_exists(dirname(__FILE__).'/lang/eng.php')) {
 // ---------------------------------------------------------
 
 // set font
-$pdf->SetFont('helvetica', '', 12);
+$pdf->SetFont('helvetica', '', 18);
 
 // --- example with background set on page ---
 
