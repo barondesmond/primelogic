33e815d2b81ff1d60874a46a1f1e063648efaff4
Baron Desmond <baron@desmond.com>
2018-11-21 13:26:14 -0600

    Update _pdf.php

--- _pdf.php
+++ _pdf.php
@@ -17,14 +17,14 @@ $pdf->SetAuthor('Baron Desmond');
 $pdf->SetTitle('pdf');
 $pdf->SetSubject('pdf');
 $pdf->SetKeywords('pdf');
-/*
+
 $pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' 001'
 , PDF_HEADER_STRING, array(0,64,255), array(0,64,128));
 
 $pdf->setFooterData(array(0,64,0), array(0,64,128));
 $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
 $pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
-*/
+
 $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
 $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
 $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
@@ -33,7 +33,7 @@ $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
 $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
 $pdf->setFontSubsetting(true);
 $pdf->SetFont('helvetica', '', 14, '', true);
-$pdf->AddPage();
+//$pdf->AddPage();
 
 //$html = '<<<EOD' . $html . 'EOD;';
 
