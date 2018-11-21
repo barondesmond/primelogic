64a4977a96b7e2503a6db47d013abfbb7ae278f0
Baron Desmond <baron@desmond.com>
2018-11-21 15:38:06 -0600

    t

--- table.php
+++ table.php
@@ -5,7 +5,7 @@ $html='<html><head></head><body style="margin: 0px;">
  table.first {
         color: black;
         font-family: helvetica;
-        font-size: 12pt;
+        font-size: 10pt;
         border-left: 3px solid red;
         border-right: 3px solid #FF00FF;
         border-top: 3px solid green;
