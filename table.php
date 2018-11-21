d8e6bb09ea7145033799aa12edb5446103ed71bd
Baron Desmond <baron@desmond.com>
2018-11-21 08:59:21 -0600

    t

--- table.php
+++ table.php
@@ -1,17 +1,3 @@
 <html><head></head><body>
-<style>
-body {
 
-background-image: url("/var/www/html/primelogic/PLIS.png");
-
-background-repeat: repeat-y no-repeat;
-
-background-color: #333;
-
-margin: 0;
-
-padding: 0;
-
-}
-</style>
-<table width="100%" border="0" cellspacing="0" cellpadding="0"> <tr><td>First column</td><td>Second column</td><td>Third column</td></tr> <tr><td>First column</td><td>Second column</td><td>Third column</td></tr> <tr><td>First column</td><td>Second column</td><td>Third column</td></tr> </table></body></html>
+<table width="100%" border="0" cellspacing="0" cellpadding="0" background="/var/www/html/primelogic/PLIS.png"> <tr><td>First column</td><td>Second column</td><td>Third column</td></tr> <tr><td>First column</td><td>Second column</td><td>Third column</td></tr> <tr><td>First column</td><td>Second column</td><td>Third column</td></tr> </table></body></html>
--- table.php.bak
+++ table.php.bak
@@ -14,4 +14,4 @@ padding: 0;
 
 }
 </style>
-<table width="100%" border="0" cellspacing="0" cellpadding="0" background="/var/www/html/primelogic/PLIS.png"> <tr><td>First column</td><td>Second column</td><td>Third column</td></tr> <tr><td>First column</td><td>Second column</td><td>Third column</td></tr> <tr><td>First column</td><td>Second column</td><td>Third column</td></tr> </table></body></html>
+<table width="100%" border="0" cellspacing="0" cellpadding="0"> <tr><td>First column</td><td>Second column</td><td>Third column</td></tr> <tr><td>First column</td><td>Second column</td><td>Third column</td></tr> <tr><td>First column</td><td>Second column</td><td>Third column</td></tr> </table></body></html>
