b5cae412d0e69d01aabb08c84f1a31a523ac696a
Baron Desmond <baron@desmond.com>
2018-11-21 16:24:20 -0600

    t

--- table.php
+++ table.php
@@ -20,7 +20,7 @@ $html='<html><head></head><body style="margin: 0px;">
   <td width="50" align="center"></td>
   <td width="350" ><color="grey">Billing Address:</color><BR><b>Lafayette Co. Chancery Clerk<BR>300 North Lama Street<BR>PO BOX 1240<BR>Oxford MS 38655<BR></b><BR><BR></td>
   <td width="200" align="left">XXXX<br />XXXX</td>
-  <td width="100" >XXXX<br />XXXX</td>
+  <td width="100" align="left">Service Location</td>
  </tr>
  <tr>
   <td width="200" align="center" rowspan="3">2.</td>
