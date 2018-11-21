f7debf01765d72f40d8722ae83be8b559f09ccf4
Baron Desmond <baron@desmond.com>
2018-11-21 13:35:40 -0600

    Update _pdf.php

--- _pdf.php
+++ _pdf.php
@@ -8,7 +8,8 @@ function htmlpdf($html, $fpdf)
 $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
 $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
-
+$pdf->SetPrintHeader(false);
+$pdf->SetPrintFooter(false);
 if (@file_exists(dirname(__FILE__).'/lang/eng.php')) {
     require_once(dirname(__FILE__).'/lang/eng.php');
     $pdf->setLanguageArray($l);
