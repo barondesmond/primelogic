3591461d721cc1b49ae87f8b6a9e2c51d6ccee9b
Baron Desmond <baron@desmond.com>
2018-11-21 09:07:37 -0600

    t

--- table.php
+++ table.php
@@ -2,7 +2,7 @@
 <style>
 body {
 
-background-image: url("PLIS.png");
+background-image: url("PL_INVOICE-construction-1.png");
 
 background-repeat: repeat-y no-repeat;
 
@@ -14,4 +14,4 @@ padding: 0;
 
 }
 </style>
-<table width="100%" border="0" cellspacing="0" cellpadding="0" background="PLIS.png"> <tr><td>First column</td><td>Second column</td><td>Third column</td></tr> <tr><td>First column</td><td>Second column</td><td>Third column</td></tr> <tr><td>First column</td><td>Second column</td><td>Third column</td></tr> </table></body></html>
+<table width="100%" border="0" cellspacing="0" cellpadding="0" background="PL_INVOICE-construction-1.png"> <tr><td>First column</td><td>Second column</td><td>Third column</td></tr> <tr><td>First column</td><td>Second column</td><td>Third column</td></tr> <tr><td>First column</td><td>Second column</td><td>Third column</td></tr> </table></body></html>
