5d19fead29065d211efd146d7c231d4db453d6cd
Baron Desmond <baron@desmond.com>
2018-11-21 12:58:45 -0600

    Update _pdf.php

--- _pdf.php
+++ _pdf.php
@@ -1,11 +1,12 @@
 <?php
+require_once('tcpdf/tcpdf_include.php');
+
 
 function htmlpdf($html, $fpdf)
 {
 
 
 // Include the main TCPDF library (search for installation path).
-require_once('tcpdf/tcpdf_include.php');
 
 // create new PDF document
 $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
