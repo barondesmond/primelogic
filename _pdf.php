a47064c0398851e73d7cd366b66e26841e0f1c0e
Baron Desmond <baron@desmond.com>
2018-11-21 14:26:41 -0600

    t

--- _pdf.php
+++ _pdf.php
@@ -5,7 +5,25 @@ require_once('../tcpdf/tcpdf.php');
 function htmlpdf($html, $fpdf)
 {
 
+ 
+
 $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+
+
+       // get the current page break margin
+        $bMargin = $pdf->getBreakMargin();
+        // get current auto-page-break mode
+        $auto_page_break = $pdf->AutoPageBreak;
+        // disable auto-page-break
+        $pdf->SetAutoPageBreak(false, 0);
+        // set bacground image
+        $img_file = 'PL_INVOICE-service-1.png';
+        $pdf->Image($img_file, 0, 0, 210, 297, '', '', '', false, 300, '', false, false, 0);
+        // restore auto-page-break status
+        $pdf->SetAutoPageBreak($auto_page_break, $bMargin);
+        // set the starting point for the page content
+        $pdf->setPageMark();
+
 $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
 $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
 $pdf->SetPrintHeader(false);
