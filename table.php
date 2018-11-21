ca320633fa849e40479bc0796845c763563b81a2
Baron Desmond <baron@desmond.com>
2018-11-21 09:46:08 -0600

    t

--- table.php
+++ table.php
@@ -1,4 +1,17 @@
 <html><head></head><body>
+<style>
+#my-bg {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 1;
+}
+#my-text {
+  color: white;
+  position: relative;
+  z-index: 2;
+}
+</style>
   <img src="PL_INVOICE-NMT-1.png" id="my-bg" />
     <p id="my-text">
 <table width="678" height="872" border="0" cellspacing="0" cellpadding="0"> <tr><td>First column</td><td>Second column</td><td>Third column</td></tr> <tr><td>First column</td><td>Second column</td><td>Third column</td></tr> <tr><td>First column</td><td>Second column</td><td>Third column</td></tr> </table>
