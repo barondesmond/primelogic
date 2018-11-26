3d32702b037d3c593e9474bf857b95331156d876
Baron Desmond <baron@desmond.com>
2018-11-26 11:38:38 -0600

    t

--- table.php
+++ table.php
@@ -2,12 +2,13 @@
 include("_pdf.php");
 include("_invoice.php");
 
-$html = invoice($invoice);
+//$html = invoice($invoice);
 if ($_GET['debug'])
 {
 	echo $html;
 	exit;
 }
+$html = "<html><head></head><body>This is a test</body></html>";
 $file = htmlpdf($html, 'test.pdf');
 //echo $file;
 ?>
