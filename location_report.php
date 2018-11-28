03ac6f787e88ad8fd2df57c80dd1cb2035bbc263
Baron Desmond <baron@desmond.com>
2018-11-28 16:07:02 -0600

    t

--- location_report.php
+++ location_report.php
@@ -10,7 +10,7 @@ CustomerInactive 0
 ReceiveNotifications -1
 EmailTasks(1-6) 2,255
 */
-define('EMAIL_SEND', '');
+define('EMAIL_SEND', 'barondesmond@gmail.com');
 
 $html = location_basis();
 //echo $html;
@@ -29,10 +29,10 @@ foreach ($sm as $emp => $emails)
 	foreach ($email_send as $send)
 	{
 		echo "Email = $send \n";
-		email_report($send, "Priority Location Invoice Email Need Fixing", $html);
+		//email_report($send, "Priority Location Invoice Email Need Fixing", $html);
 	}
 }
 
-email_report('dispatch@plisolutions.com', "Priority Location Invoice Email Need Fixing", $html);
+email_report('barondesmond@gmail.com', "Priority Location Invoice Email Need Fixing", $html);
 ?>
 
