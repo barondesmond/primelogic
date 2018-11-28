6985265fdbdf6a75389157694d861ccef0aab8c3
Baron Desmond <baron@desmond.com>
2018-11-28 08:19:40 -0600

    t

--- _db_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-echo "test";
-$server_instance = 'rds01.plis.local\esc';
-$user = 'Reports';
-$pass = 'Reports123!';
-$con = @mssql_connect($server_instance, $user, $pass) or die('Error connecting to ' . $server_instance);
-
-?>
