8c5ecbb2fca4bc6633bd877203a0b68cb24a61db
Baron Desmond <baron@desmond.com>
2018-11-29 14:01:51 -0600

    t

--- qu.php
+++ qu.php
@@ -3,16 +3,6 @@ include("_db_config.php");
 
 if ($argv[1])
 {
-	$x=0;
-	echo $argv[1] . "\r\n";
-	$ar = explode(';', $argv[1]);
-	if (count($ar)>1)
-	{
-		echo $ar[0];
-		$res = mssql_query($ar[0]);
-		$argv[1] = $ar[1];
-	}
-	$res = mssql_query($argv[1]);
 	while ($db = mssql_fetch_array($res))
 	{
 		//print_r($db);
@@ -34,14 +24,6 @@ if ($argv[1])
 			}		
 			echo "\r\n";
 		
-		}
-		if (!isset($argv[2]))
-		{
-			for ($i=0;$i< count($hd); $i++)
-			{
-				echo "$db[$i],";
-			}
-			echo "\r\n";
 		}
 	}
 
