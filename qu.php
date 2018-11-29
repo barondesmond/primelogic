81ca88a2f55a344b102cfa76ff3e19d29585fc0c
Baron Desmond <baron@desmond.com>
2018-11-29 09:00:45 -0600

    t

---
+++ qu.php
@@ -0,0 +1,31 @@
+<?php
+
+if ($argv[1])
+{
+	$res = db_query($sql);
+	while ($db = mssql_fetch_array($res)
+	{
+		if (!$hd)
+		{
+			foreach ($db, $key=> $value)
+			{
+				if (is_numeric($key))
+				{
+					$hd[] = $value;
+					echo "$value,";
+				}
+			}		
+			echo "\r\n";
+		$hd = '1';
+		}
+		for ($i=0;$i< count($hd); $i++)
+		{
+			echo "$db[$i],";
+		}
+		echo "\r\n";
+
+	}
+
+}	
+
+?>
