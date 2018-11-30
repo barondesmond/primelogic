22070f7fbb8bd8d9eedb663504d66e61046b4828
Baron Desmond <baron@desmond.com>
2018-11-30 08:24:43 -0600

    t

--- qu.php
+++ qu.php
@@ -8,16 +8,18 @@ if ($argv[1])
 if ($_GET['query'])
 {
 	$query = $_GET['query'];
-
-	$res = mssql_query($argv[1]);
+}
+	
+	
+	$res = mssql_query($query);
 	$x=0;
 	while ($db = mssql_fetch_array($res, MSSQL_ASSOC))
 	{
 		show_data($db);
 	}
-	show_data(array((), '1');
+	show_data(array(), '1');
 
 
-}	
+
 
 ?>
