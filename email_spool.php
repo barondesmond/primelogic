36123fd5a04443a5597fda357cf426c52e76fad8
Baron Desmond <baron@desmond.com>
2018-12-03 16:01:16 -0600

    t

--- email_spool.php
+++ email_spool.php
@@ -1,18 +1,36 @@
 <?php
 //email spool cron
 
-
+$start = time();
+$end = time() . 86000;
 include("_email.php");
 
 define('SPOOLREAD', 'read');
-
+while (time() < $end)
+{
 if ($handle = opendir('/var/www/email')) {
     while (false !== ($entry = readdir($handle))) {
         if ($entry != "." && $entry != "..") {
+
+			send_json_file($entry);
+			
             echo "$entry\n";
         }
     }
     closedir($handle);
+
 }
+
+
+function send_json_file($entry)
+{
+	$st = fopen($entry, "r");
+	$json = fread($st);
+
+	$db = json_decode($json);
+	print_r($db);
+	fclose($st);
+	exit;
+}	
 
 ?>
