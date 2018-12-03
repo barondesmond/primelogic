558dc633b54ad4e8571b5c88923fdc15e1371631
Baron Desmond <baron@desmond.com>
2018-12-03 10:30:02 -0600

    t

--- _report.php
+++ _report.php
@@ -385,6 +385,7 @@ $res = mssql_query($sql);
 						$t['fnchg'] = 'Finance Charges';
 						$html .= table_hd($t, '', '', count($ik));				
 						$html .= "\r\n";
+						$html .= $finchrg;
 					}
 					$loc = '<b>' . $db['LastName'] .'</b>' . "<BR>Location: " . $db['LocName'];
 					$x[$loc] = $loc;
