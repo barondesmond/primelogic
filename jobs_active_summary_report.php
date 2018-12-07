e9312a1416c6c16c91dd7d937b9b41e14f3a1edf
Baron Desmond <baron@desmond.com>
2018-12-07 10:35:24 -0600

    t

--- jobs_active_summary_report.php
+++ jobs_active_summary_report.php
@@ -34,7 +34,7 @@ for ($i=0; $i < count($jobs); $i++)
 		$table .= job_hd($key);
 		$table .= job_bar($key);
 	}
-	$table .= job_title($td[0]);
+	$table .= job_title($job);
 	for ($t=0; $t< count($td); $t++)
 	{
 		$table .= job_row($td[$t], $key);
