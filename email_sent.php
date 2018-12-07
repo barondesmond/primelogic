5d9211ae45fb861369460449e003cda81447ff19
Baron Desmond <baron@desmond.com>
2018-12-07 15:37:44 -0600

    t

--- email_sent.php
+++ email_sent.php
@@ -5,7 +5,7 @@ $salt = 'wtf';
 
 if (email_validate($_GET['email']) && $_GET['job'] != '')
 {
-	$res = system("/usr/bin/php /usr/bin/php/job_detail_report.php '" . $_GET['job'] . "'" .  ' ' . "'" . $_GET['email'] . "'");
+	$res = system("/usr/bin/php /var/www/html/primelogic/job_detail_report.php '" . $_GET['job'] . "'" .  ' ' . "'" . $_GET['email'] . "'");
 
 	echo "Email sent to " . $_GET['email'];
 }
