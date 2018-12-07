8068b11acfc627547e95d8980676d3589c19e97a
Baron Desmond <baron@desmond.com>
2018-12-07 16:02:01 -0600

    t

--- _email.php
+++ _email.php
@@ -76,7 +76,7 @@ function email_report($email, $subject, $body, $filename='', $cid='', $name='', 
 			$db[$er_array[$i]] = ${$er_array[$i]};
 		}
 		$enc =json_encode($db);
-		$file = '/var/www/html/primelogic/email/' . time() . '.' . $email . '.' . urlencode($subject) .  '.email';
+		$file = DIRD . time() . '.' . $email . '.' . urlencode($subject) .  '.email';
 		$stream = fopen($file, 'w');
 		fwrite($stream, $enc);
 		fclose($stream);
