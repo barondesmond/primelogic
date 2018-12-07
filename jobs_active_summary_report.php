ccd4ded796a59ce2b9b03d6b41fecf5a14ea6064
Baron Desmond <baron@desmond.com>
2018-12-07 09:47:10 -0600

    t

--- jobs_active_summary_report.php
+++ jobs_active_summary_report.php
@@ -6,7 +6,7 @@ include("_job.php");
 
 $jobs = jobs_active_query();
 print_r($jobs);
-foreach ($jobs as $job->$db)
+foreach ($jobs as $job=>$db)
 {
 	$gr = job_query($job);
 	$td = job_sommary($gr);
