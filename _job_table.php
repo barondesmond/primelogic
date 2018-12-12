429d79b2a60278222141bb30fcb910e630cf3318
Baron Desmond <baron@desmond.com>
2018-12-12 13:02:07 -0600

    t

--- _job_table.php
+++ _job_table.php
@@ -42,7 +42,7 @@ return $table;
 function job_bar_dot($key)
 {
 	$ct = count($key);
-	$table = '<tr><td colspan="' . $ct . '" style="border-bottom-style: solid; color= grey;"></tr>';
+	$table = '<tr><td colspan="' . $ct . '" style="border-bottom: solid 1px gray;"></tr>';
 return $table;
 }	
 	
