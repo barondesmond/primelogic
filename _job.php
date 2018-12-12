88a339c9f0049e8adc5a98ef6139f0cb31622796
Baron Desmond <baron@desmond.com>
2018-12-12 10:57:33 -0600

    t

--- _job.php
+++ _job.php
@@ -7,7 +7,7 @@
 function job_query($val='J-0001907', $action = '')
 {
 
-		$query = "SELECT TransDesc as Document, TransDate, TransType as Type, Amount as Amount, Units as Units, Account, Source, CostType, CASE WHEN CostType = '0' THEN 'Income' WHEN CostType = '200' THEN 'Labor' WHEN CostType = '100' THEN 'Material' WHEN CostType = '150' THEN 'Equipment' WHEN CostType='300' THEN 'Other300' WHEN CostType='500' THEN 'Other'  ELSE ''END as CostGroup, [DESC]
+		$query = "SELECT TransType as Type, TransDesc as Document, TransDate, Amount as Amount, Units as Units, Account, Source, CostType, CASE WHEN CostType = '0' THEN 'Income' WHEN CostType = '200' THEN 'Labor' WHEN CostType = '100' THEN 'Material' WHEN CostType = '150' THEN 'Equipment' WHEN CostType='300' THEN 'Other300' WHEN CostType='500' THEN 'Other'  ELSE ''END as CostGroup, [DESC]
 		FROM Jobs 
 		INNER JOIN FinLedger ON Jobs.JobID = FinLedger.JobID
 		INNER JOIN COA ON FinLedger.AccountID = COA.AccountID
