4d4cbcfa0c1a42ad509a8067ce259ea63503d248
Baron Desmond <baron@desmond.com>
2018-12-21 10:47:09 -0600

    t

--- employees_json.php
+++ employees_json.php
@@ -7,9 +7,12 @@ $js['title'] = 'Employee List';
 $js['description'] = 'Employee Number, Email, Phone for authentication';
 $sql = "SELECT EmpName, Email, phone FROM Employee WHERE Email != '' and Inactive = '0'";
 $res = mssql_query($sql);
+$i=1;
 while ($db = mssql_fetch_assoc($res))
 {
+	$db['id'] = $i;
 	$js[employees][] = $db;
+	$i++;
 }
 
 header('Content-Type: application/json');
