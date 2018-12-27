cabda27ab39cf15e15b6a122a73186f8e5503b5e
Baron Desmond <baron@desmond.com>
2018-12-27 08:16:52 -0600

    t

---
+++ email.php
@@ -0,0 +1,12 @@
+<?
+include("_email.php");
+
+$email = $argv[1];
+$subject = $argv[2];
+$body = $argv[3];
+$pdf = $argv[4];
+$cid = '';
+$name = '';
+$func = '';
+
+email_report($email, $subject, $body, $filename='', $cid='', $name='', $pdf = '', $func = '' )
