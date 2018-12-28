e3bb6d56596b5f1ee90270081ce7e937b2e97c90
Baron Desmond <baron@desmond.com>
2018-12-28 16:01:28 -0600

    t

--- employees_json.php
+++ employees_json.php
@@ -2,10 +2,13 @@
 include("_db_config.php");
 //api app
 
-
+if ($_REQUEST[Name] && $_REQUEST['Email'})
+{
+	$sel = " and EmpName = '". $ _GET['Name'] . ' and Email = ' . $_GET['Email'] . "'";
+}
 $js['title'] = 'Employee List';
 $js['description'] = 'Employee Number, Email, Phone for authentication';
-$sql = "SELECT EmpNo as id, EmpName, Email, phone FROM Employee WHERE Email != '' and Inactive = '0'";
+$sql = "SELECT EmpNo as id, EmpName, Email, phone FROM Employee WHERE Email != '' and Inactive = '0' $sel";
 $res = mssql_query($sql);
 $i=1;
 while ($db = mssql_fetch_assoc($res))
