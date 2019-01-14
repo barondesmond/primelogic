b98b7b834ad6512532c3f5faef89603a3a669f5d
Baron Desmond <baron@desmond.com>
2019-01-14 15:34:39 -0600

    t

--- upload/index.php
+++ upload/index.php
@@ -1,12 +1,12 @@
 <?php
 include("../_db_config.php");
 $req = file_get_contents('php://input');
-
+error_log($req);
 $file = fopen('test' . '.file', 'w');
 fwrite($file, $req);
 fclose($file);
 
 
 header('Content-Type: application/json');
-$db['location'] = 'https://' . HOST . '/upload/' . $_POST['name'];
+$db['location'] = 'https://' . HOST . '/upload/photo.jpg' ;
 echo json_encode($db);
