15e82bd8fdc321ffd0c7461b5496d1746ad12cc8
Baron Desmond <baron@desmond.com>
2019-01-14 09:29:24 -0600

    t

--- location_json.php
+++ location_json.php
@@ -128,7 +128,7 @@ function distance($lat1, $lon1, $lat2, $lon2) {
 
 if ( $_REQUEST['LocName'])
 {
-	$_REQUEST['LocName'] = str_replace("'", "\\'", $_REQUEST['LocName']));
+	$_REQUEST['LocName'] = str_replace("'", "\\'", $_REQUEST['LocName']);
 
 	$sql = "SELECT Street, City,State,Zip FROM Location WHERE LocName = '" . $_REQUEST['LocName'] . "'";
 	$res = mssql_query($sql);
