48b7175fe208fba0271df38914903b99d5e8df4f
Baron Desmond <baron@desmond.com>
2019-01-15 10:33:16 -0600

    t

--- authempinst_json.php
+++ authempinst_json.php
@@ -25,6 +25,7 @@ if ($_REQUEST['checkinStatus'] == 'Stop')
 elseif ($_REQUEST['checkinStatus'] == 'Start')
 {
 	$_REQUEST['StartTime'] = $time;
+	$_REQUEST['EmpActive'] = '1';
 	$array = array('EmpNo', 'InstallationId', 'Name', 'latitude', 'longitude', 'event', 'StartTime', 'EmpActive', 'violation', 'image');
 	foreach ($array as $key)
 	{
