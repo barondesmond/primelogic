595d053777a2115d6d511d1f53dd6579a4a410f1
Baron Desmond <baron@desmond.com>
2019-01-25 13:21:17 -0600

    t

--- document.php
+++ document.php
@@ -6,12 +6,11 @@ if ($_REQUEST['Name'])
  $sql = "SELECT * FROM DocAttach WHERE Name = '" . $_REQUEST['Name'] "'";
  $res = mssql_query($sql);
  $db = mssql_fetch_array($res, MSSQL_ASSOC);
- if ($db['Extension'])
- {
-     header("Content-type:application/pdf");
-	 header("Content-Disposition:attachment;filename='" . $db['Name'] . $db['Extension'] . "'");
-	 echo $db['Document'];
-	 exit;
+	if ($db['Extension'])
+	{
+	  header("Content-type:application/pdf");
+		 header("Content-Disposition:attachment;filename='" . $db['Name'] . $db['Extension'] . "'");
+		 echo $db['Document'];
+		 exit;
+	}
  }
- 
-
