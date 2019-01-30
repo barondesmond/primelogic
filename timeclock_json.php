0625574be86284b216e7537dd8c6937c254b4749
Baron Desmond <baron@desmond.com>
2019-01-30 16:38:52 -0600

    t

--- timeclock_json.php
+++ timeclock_json.php
@@ -3,6 +3,8 @@ include("_db_config.php");
 
 $sql = "SELECT * FROM TimeClockApp WHERE Posted = ''";
 $res = mssql_query($sql);
+$data['error'][] = mssql_get_last_message();
+$data['error'][] = $sql;
 while ($db = mssql_fetch_array($res, MSSQL_ASSOC))
 {
 	$data['TimeClock'][] = $db;
