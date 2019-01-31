b9e1674d74ef78641b90ae5130e0a71f0a635c5a
Baron Desmond <baron@desmond.com>
2019-01-31 14:42:28 -0600

    t

--- timeclock_json.php
+++ timeclock_json.php
@@ -1,10 +1,16 @@
 <?php
 include("_db_config.php");
 
-$sql = "SELECT * FROM TimeClockApp
-INNER JOIN Employee ON TimeClockApp.EmpNo = Employee.EmpNo
-INNER JOIN Jobs ON TimeClockApp.Name = Jobs.Name
-INNER JOIN Location ON Jobs.CustNo = Location.CustNo and Jobs.LocNo = Location.LocNo
+$sql = "SELECT TImeClockApp.TimeClockID, Employee.EmpNo as EmpNo, Employee.EmpName, Employee.Email, UserAppAuth.installationId, UserAppAuth.authorized, TimeClockApp.EmpActive, TimeClockApp.Screen, TimeClockApp.event, TimeClockApp.Name, TimeClockApp.Dispatch, Location.LocName, Jobs.JobNotes, LocationApi.latitude, LocationApi.longitude, TimeClockApp.Screen, Dispatch.Dispatch, DispLoc.LocName as DispatchName, Dispatch.Notes as DispatchNotes, DispLocApi.longitude as dispatchlongitude, DispLocApi.latitude as dispatchlatitude, DispLoc.Add1, DispLoc.Add2, DispLoc.City, DispLoc.State, DispLoc.Zip, DispLoc.Phone1  FROM Employee
+INNER JOIN UserAppAuth ON Employee.EmpNo = UserAppAuth.EmpNo
+LEFT JOIN TimeClockApp ON Employee.EmpNo = TimeClockApp.EmpNo 
+LEFT JOIN Jobs" . $dev . " as Jobs ON Jobs.Name = TimeClockApp.Name 
+LEFT JOIN Location ON Jobs.CustNo = Location.CustNo and Jobs.Location = Location.LocNo
+LEFT JOIN LocationApi ON Location.LocName = LocationApi.LocName
+LEFT JOIN Dispatch" . $dev . " as Dispatch ON TimeClockApp.Dispatch = Dispatch.Dispatch 
+LEFT JOIN DispTech" . $dev . " as DispTech ON Dispatch.Dispatch = DispTech.Dispatch and TimeClockApp.event = DispTech.Status 
+LEFT JOIN Location as DispLoc ON Dispatch.CustNo = DispLoc.CustNo and Dispatch.LocNo = DispLoc.LocNo 
+LEFT JOIN LocationApi as DispLocApi ON DispLoc.LocName = DispLocApi.LocName
 WHERE Posted is NULL";
 $res = mssql_query($sql);
 $data['error'][] = mssql_get_last_message();
