e2e7fd83c5c26c647c4533ea7b3affcd4c6415f4
Baron Desmond <baron@desmond.com>
2019-02-15 14:27:52 -0600

    t

--- _query.php
+++ _query.php
@@ -13,7 +13,7 @@ function dbn_table_column($db, $var2, $dl=array())
 
 	$res3 = @mssql_query($sql3);
 	
-		$db2 = @mssql_fetch_array($res3, MSSQL_ASSOC))
+		$db2 = @mssql_fetch_array($res3, MSSQL_ASSOC)
 		if ($db2[total] > 0)
 		{
 			$dl[$db['DBN']][$db['TableName']][$db['ColumnName']] = $var2;
