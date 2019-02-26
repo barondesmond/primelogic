661412f2f771eefe7b28da7bc414e4a14920666a
Baron Desmond <baron@desmond.com>
2019-02-26 12:16:35 -0600

    t

--- show_invoice.php
+++ show_invoice.php
@@ -3,6 +3,6 @@ if ($_REQUEST['Invoice'])
 {
 	$file = '/var/www/pdf/' . $_REQUEST['Invoice'] . '.pdf';
 	header("Content-type:application/pdf");
-	header("Content-Disposition:inline;filename='$file'");
+	header("Content-Disposition:attachment;filename='" . $_REQUEST['Invoice'] . ".pdf'");
 	readfile($file);	
 }
