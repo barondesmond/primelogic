b715550beb6beec4bbf8a08e521e093eb022ad68
Baron Desmond <baron@desmond.com>
2019-02-26 10:08:02 -0600

    t

--- _location_api.php
+++ _location_api.php
@@ -33,7 +33,7 @@ function mapquest_match($resp, $db)
 	$loc = $resp['results']['0']['locations']['0'];
 	//print_r($loc);
 	//print_r($db);
-	if (trim($loc['street']) == trim($db['Add1']))
+	if (trim($loc['street']) == trim($db['Add1']) || $loc['geocodeQualityCode'] == 'P1AAA')
 	{
 			//match
 		$match = $loc;
