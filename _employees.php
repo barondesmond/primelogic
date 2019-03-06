90d6055ef866ee44f4fb22d3b5617135bb751c33
Baron Desmond <baron@desmond.com>
2019-03-06 10:39:08 -0600

    t

--- _employees.php
+++ _employees.php
@@ -59,7 +59,7 @@ $js['description'] = 'JobGroupID, EmpNo, Job';
 $sql = "SELECT ge.JobGroupID, ge.EmpNo, je.Job, JobGroup, Location.LocName FROM JobGroupEmployee as ge INNER JOIN JobGroupEmployee as je ON ge.JobGroupID = je.JobGroupID
 INNER JOIN JobGroup ON je.JobGroupID = JobGroup.JobGroupID
 INNER JOIN Jobs ON je.Job = Jobs.Name 
-INNER JOIN Location on Jobs.CustNo = Location.CustNo and Jobs.LocNo = Location.LocNo
+INNER JOIN Location ON Jobs.CustNo = Location.CustNo and Jobs.Location = Location.LocNo
 $sel ORDER BY ge.JobGroupID ASC ";
 $res = mssql_query($sql);
 $i=1;
