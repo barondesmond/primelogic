7ecaba469c60aed29614e83b24844ffd168aff4d
Baron Desmond <baron@desmond.com>
2019-03-06 12:31:48 -0600

    t

--- assign_json.php
+++ assign_json.php
@@ -29,6 +29,44 @@ if (isset($_REQUEST['add_job_group']))
 	echo json_encode($js);
 	exit;
 }
+if (isset($_REQUEST['delete_job_group_employee']))
+{
+	if (isset($_REQUEST['JobGroup'])
+	{
+		foreach ($_REQUEST['JobGroup'] as $JobGroup)
+		{
+			if (isset($_REQUEST['Employee']))
+			{
+				foreach ($_REQUEST['Employee']	as $EmpNo)
+				{
+					$sql = "DELETE FROM JobGroupEmployee WHERE JobGroupID = '$JobGroup' and EmpNo = '$EmpNo'";
+					$res = mssql_query($sql);
+					$mes = mssql_get_last_message();
+					if ($mes != '')
+					{
+						$js['error'][] = $mes; 
+						$js['error'][] = $sql;						}
+					}
+				}
+			}			
+			if (isset($_REQUEST['Job']))
+			{
+				foreach ($_REQUEST['Job'] as $Job)
+				{
+					$sql = "DELETE FROM JobGroupEmployee WHERE JobGroupID = '$JobGroup' and Job =  '$Job'";
+					$res = mssql_query($sql);
+					$mes = mssql_get_last_message();
+					if ($mes != '')
+					{
+						$js['error'][] = $mes;
+						$js['error'][] = $sql;
+					}
+				}
+			}
+		}
+
+	}
+}
 
 if (isset($_REQUEST['add_job_group_employee']))
 {
