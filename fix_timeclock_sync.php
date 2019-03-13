6ccefcd8b471a1ebff5b5639be0a86f3e826611f
Baron Desmond <baron@desmond.com>
2019-03-13 14:41:48 -0500

    t

---
+++ fix_timeclock_sync.php
@@ -0,0 +1,42 @@
+<?php
+include("_db_config.php");
+
+
+function convert_date_time($date, $time)
+{
+	$expday = explode(' ', $date);
+	$day = $expday[0] . ' ' . $expday[1] . ' ' . $expday[2] . 	$time;
+	$StartTime = strtotime($day);
+	if ($StartTime > 0)
+	{
+		return $StartTime;
+	}
+return false;
+}
+
+
+//Fix Sync Errors Dispatch
+
+$sql = "SELECT UserAppAuth.*, DispTech.* FROM UserAppAuth
+INNER JOIN DispTech ON EmpNo = ServiceMan 
+LEFT JOIN TimeClockApp ON UserAppAuth.EmpNo = TimeClockApp.EmpNo and EmpActive = '1'
+WHERE DispTech.Status IN ('Traveling', 'Working') and TimeClockApp.EmpNo is NULL";
+
+$res = mssql_query($sql);
+while ($db = mssql_fetch_assoc($res))
+{
+	print_r($db);
+
+	if ($db['DispDate'] != '' && $db['TimeOn'] != '' && $db['TimeOff'] == '' && $db['DateOff'] == '')
+	{
+		$db['Screen'] = 'Dispatch';
+		$db['event'] = $db['Status'];
+		$db['EmpActive'] = '1';
+		if ($db['StartTime'] = convert_date_time($db['DispDate'], $db['TimeOn']))
+		{
+			print_r($db);
+			exit;
+		}
+	}
+
+}
