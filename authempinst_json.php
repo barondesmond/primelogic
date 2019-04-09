85d10528a73fd7ed88f068bcae2d0a82866f2793
Baron Desmond <baron@desmond.com>
2019-04-09 14:57:42 -0500

    t

--- authempinst_json.php
+++ authempinst_json.php
@@ -295,16 +295,8 @@ $db = mssql_fetch_array($res, MSSQL_ASSOC);
 	{
 		$db = dispatch_hours($db, $dev);
 	}
-	if ($db['LocName'] != '')
-	{
-		$loc = location_api($db['LocName'], $db);
-	}
-	if ($db['DispatchName'] != '')
-	{
-		$loc = location_api($db['LocName'], $db);
-	}
-		$db['latitude'] = $loc['latitude'];
-		$db['longitude'] = $loc['longitude'];
+
+	
 
 return $db;
 }
