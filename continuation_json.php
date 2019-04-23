d2309b731154c1167d4ed0444edc891d3fc4c319
Baron Desmond <baron@desmond.com>
2019-04-23 16:40:08 -0500

    t

--- continuation_json.php
+++ continuation_json.php
@@ -42,7 +42,7 @@ for ($page = 1; $page < $pages; $page++)
 			}
 			elseif ($col =='8')
 			{
-				$db[$page][$row][$col] = '%' . round($db[$page][$row][7] / $db[$page][$row][3], 2);
+				$db[$page][$row][$col] = round($db[$page][$row][7] / $db[$page][$row][3], 2);
 		
 			}
 			elseif ($col =='9' )
@@ -52,12 +52,20 @@ for ($page = 1; $page < $pages; $page++)
 			}
 			elseif ($col =='10' )
 			{
-				$db[$page][$row][$col] = '%' . round($db[$page][$row][7] * ($db[$page][$row][12]/100));
-
+				$db[$page][$row][$col] = round($db[$page][$row][7] * ($db[$page][$row][12]/100));
+				
 			}
 			if ($db[$page][$row][$col] == '0')
 			{
-					//$db[$page][$row][$col] = '';
+					$db[$page][$row][$col] = '';
+			}
+			elseif ($col=='8')
+			{
+				$db[$page][$row][$col] = '%' . $db[$page][$row][$col] ;
+			}
+			elseif ($col=='10')
+			{
+				$db[$page][$row][$col] = '%' . $db[$page][$row][$col] ;
 			}
 	
 		}
