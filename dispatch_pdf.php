e09b844bd74b86ce5aaba31730bffb27b5b0947e
barondesmond <baron@desmond.com>
2019-08-15 10:53:14 -0500

    t

--- dispatch_pdf.php
+++ dispatch_pdf.php
@@ -105,6 +105,8 @@ $pdf->setPageMark();
 
 	$dbs = dispatch_init($dbs, $arrays[0]);
 $img_file = '/var/www/html/primelogic/upload/' . $dbs['signature'];
+echo $img_file;
+exit;
 if (!file_exists($img_file))
 	{
 	echo $img_file;
