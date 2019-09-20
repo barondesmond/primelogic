1895daa6f930363888df72447b92759ec64aff9a
barondesmond <baron@desmond.com>
2019-09-20 11:26:36 -0500

    t

--- dispatch.php
+++ dispatch.php
@@ -18,13 +18,16 @@ if ($_GET['Dispatch'])
 {
 	$file = pdf_input($_GET['Dispatch']);
 
-
-header("Content-type:application/pdf");
-header("Content-Disposition:attachment;filename='downloaded.pdf'");
-readfile("$file");
-
-
 }
+
+if (!$argv[1])
+{
+	header("Content-type:application/pdf");
+	header("Content-Disposition:attachment;filename='downloaded.pdf'");
+	readfile("$file");
+}
+
+
 email_report("barondesmond@gmail.com", "Dispatch Ticket " . $_GET['Dispatch'] , "Dispatch Ticket Attached " . $_GET['Dispatch'], $ll['filename'], $ll['cid'], $ll['name'], $file);
 
 ?>
