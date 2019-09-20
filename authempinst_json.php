97e5e12369e5b90fbe0d781fbd6d2dbaaeab8801
barondesmond <baron@desmond.com>
2019-09-20 10:57:37 -0500

    t

--- authempinst_json.php
+++ authempinst_json.php
@@ -2,7 +2,11 @@
 include("_db_config.php");
 include("_location_api.php");
 include("_dispatch.php");
+include('_report.php');
+include ('_pdf.php');
+include ('_email.php');
 include("_user_app_auth.php");
+include("dispatch_pdf.php");
 
 $auth = UserAppAuth($_REQUEST);
 if ($auth['authorized'] != '1')
