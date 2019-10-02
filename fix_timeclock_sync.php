340e7fa49d765625ae5a6ee7db13889ba5c84586
barondesmond <baron@desmond.com>
2019-10-02 10:48:56 -0500

    t

--- fix_timeclock_sync.php
+++ fix_timeclock_sync.php
@@ -8,6 +8,21 @@ if ($argv['1'])
 	$dev = 'Dev';
 }
 
+$sql = "SELECT * FROM TimeClockApp
+INNER JOIN DispTech ON TimeClockApp.Dispatch = DispTech.Dispatch and TimeClockApp.Counter = DispTech.Counter
+ WHERE EmpActive = '1' and TimeClockApp.event != DispTech.Status and StopTime is NULL";
+ $res = mssql_query($sql);
+ while ($db = mssql_fetch_assoc($res))
+ {
+	 if ($db['event'] == 'Working')
+	 {
+		 $db['StopTime'] = mktime($db['DateOff']);
+		 $sql = "UPDATE TimeClockApp SET StopTime = '"  . $db['StopTime'] . "' WHERE TimeClockID = '" . $db['TimeClockID']  . "'";
+		 echo $sql;
+	 }
+ }
+ exit;
+
 $sql = "SELECT UserAppAuth.*, DispTech.*  FROM UserAppAuth
 INNER JOIN DispTech$dev as DispTech ON UserAppAuth.EmpNo = DispTech.ServiceMan and DispDate > DATEADD(day, -15, getdate())
 LEFT JOIN TimeClockApp ON UserAppAuth.EmpNo = TimeClockApp.EmpNo and Disptech.Dispatch = TimeClockApp.Dispatch and DispTech.Counter = TimeClockApp.Counter
