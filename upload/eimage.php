198d8a21c25a20334a09cb106aa9ea24c4d448ca
barondesmond <baron@desmond.com>
2019-12-13 09:21:08 -0600

    t

--- upload/eimage.php
+++ upload/eimage.php
@@ -5,11 +5,12 @@ include("../_location_api.php");
 
 //$file =  $_REQUEST['file'];
 $send = 'barondesmond@gmail.com';
-$file = '/var/www/html/primelogic/' . $argv[1];
+$file = '/var/www/html/primelogic/upload/' . $argv[1];
 $db = location_parse_file($argv[1]);
-
+echo $file;
 if (file_exists($file))
 {
+	echo "sending file" . $file;
 	$html = "You can download image <A HREF=https://" . HOST . "/primelogic/upload/" . $argv[1] . ">here</a>";
 	$img[] = $file;
 	email_report($send, "Dispatch Image " . $db['reference'], $html, $img);
