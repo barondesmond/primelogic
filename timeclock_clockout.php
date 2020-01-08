8ea8e6e6e7667ae10d3d7bfd02ef8d5a2ebfb7e0
barondesmond <baron@desmond.com>
2020-01-08 07:59:01 -0600

    Create timeclock_clockout.php

---
+++ timeclock_clockout.php
@@ -0,0 +1,44 @@
+<?php
+include("_db_config.php");
+include("_timeclockapp.php");
+
+
+if ($argv['1'])
+{
+	$dev = 'Dev';
+}
+
+echo "Jan 25 2019 12:00:00:000AM";
+
+
+$newdate = date("Y-m-d", time()) . ' 00:00:00';
+echo "the day is $newdate";
+$yestertime = strtotime($newdate)-60;
+$yesterdate = date("M d Y ", $yestertime) . '12:00:00:000AM';
+echo "\r\nYesterdate is " . $yesterdate;
+$sql = "SELECT TimeClockApp.* FROM Time.dbo.TimeCLockApp
+WHERE event IN ('Traveling', 'Working') and TimeClockApp.EmpNo is Not Null and StartTime < '$yestertime' and EmpActive = '1'";
+echo $sql;
+$res = mssql_query($sql);
+while ($db = mssql_fetch_array($res, MSSQL_ASSOC))
+{
+	unset($db['TimeClockID']);
+	$db['installationId'] = $db['installationID'];
+	$db['checkinStatus'] = 'Stop';
+	print_r($db);
+
+if ($db['Screen'] == 'Dispatch')
+{
+	$error = dispatch_db($db, $dev, $yestertime );
+	$error2 = timeclock_db($db, $yestertime);
+	$error = array_merge($error, $error2);
+}
+else
+{
+	$error = timeclock_db($db);
+	
+
+}
+print_r($error);
+
+}
