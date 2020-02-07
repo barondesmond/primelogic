4adf9e4673ff72f5d24b8e1b1299eb6ab7285046
Baron Desmond <baron@desmond.com>
2020-02-07 08:04:17 -0600

    t

--- json.php
+++ json.php
@@ -1,25 +1,21 @@
 <?php
 include("_db_config.php");
 include("_query.php");
-if ($argv[1])
+
+if (isset($argv[1]))
 {
 	$query = $argv[1];
-
 }
 if (isset($_REQUEST['query']))
 {
-	$query = $_GET['query'];	
-	
-	$exp = query_export($query);
-	if (isset($_REQUEST['json']))
-	{
-		header('Content-Type: application/json');
-		echo json_encode($exp);
-	}
+	$query = $_REQUEST['query'];	
 }	
 
-
-
-
-
+$exp = query_export($query);
+if (isset($_REQUEST['json']))
+{
+	header('Content-Type: application/json');
+	echo json_encode($exp);
+}
+	
 ?>
